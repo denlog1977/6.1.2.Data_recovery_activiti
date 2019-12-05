83364c6a53589e2bedf1b3e5b13c288c6ccb6223
denlog1977 <denlog1977@gmail.com>
2019-12-05 09:13:22 +0300

    1

--- app/src/main/java/com/example/a612data_recovery_activiti/MainActivity.java
+++ app/src/main/java/com/example/a612data_recovery_activiti/MainActivity.java
@@ -6,10 +6,12 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -28,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private SimpleAdapter listContentAdapter;
     private SwipeRefreshLayout swipeLayout;
     private ListView listView;
+    private ArrayList<Integer> removedListItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mySharedPref = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+
+        removedListItems = new ArrayList<>();
 
         getTextFromSharedPref();
 
@@ -58,17 +63,9 @@ public class MainActivity extends AppCompatActivity {
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-//                // параметр view - строка, на которую кликнул пользователь, можно получить текст из нее
-//                String textListItem = ((TextView) view.findViewById(R.id.textView)).getText().toString();
-//                String length=((TextView) view.findViewById(R.id.textView2)).getText().toString();
-//                Toast.makeText(getApplicationContext(), textListItem + "\n[длина текста = " + length + "]", Toast.LENGTH_SHORT).show();
-                // position - номер строки, можно получить данные по этому номеру и взять текст из них
-//                String textListItem = ((Map) listContentAdapter.getItem(position)).get("text").toString();
-//                String length = ((Map) listContentAdapter.getItem(position)).get("length").toString();
-//                Toast.makeText(getApplicationContext(), textListItem + "\n[длина текста = " + length + "]", Toast.LENGTH_SHORT).show();
                 content.remove(position);
                 listContentAdapter.notifyDataSetChanged();
-//                Toast.makeText(getApplicationContext(), " --- contentSize --- " + Integer.toString(content.size()), Toast.LENGTH_SHORT).show();
+                removedListItems.add(position);
             }
         });
 
@@ -113,6 +110,23 @@ public class MainActivity extends AppCompatActivity {
     private void getTextFromSharedPref(){
         textFromSharedPref = mySharedPref.getString(LARGE_TEXT, "");
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Toast.makeText(this, "onSaveInstanceState" + "  outState.putIntegerArrayList(removedListItems)", Toast.LENGTH_SHORT).show();
+        Log.i("Lifecycle_DEN", "Сработало событие " + "onSaveInstanceState");
+        outState.putIntegerArrayList("removedListItems",removedListItems);
+    }
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        removedListItems = savedInstanceState.getIntegerArrayList("removedListItems");
+        for (int i=0 ; i<removedListItems.size(); i++) {
+            content.remove(removedListItems.get(i).intValue());
+        }
+    }
+
 
 
 }
